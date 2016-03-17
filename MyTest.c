7237aefb4423bf08a7e325b918c121b6a09497de
ZefengWang <627132520@qq.com>
2016-03-17 11:26:23 +0800

    windows programing
    none

---
+++ MyTest.c
@@ -0,0 +1,11 @@
+#include <stdio.h>  
+int main(void)  
+{  
+	char buff[10];  
+	memset(buff,0,sizeof(buff));   
+	gets(buff);  
+	printf("\n The buffer entered is [%s]\n",buff); 
+
+	system("Pause");
+	return 0;  
+}
