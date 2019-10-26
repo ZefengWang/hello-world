526b20565dd63ee423cf9bdab5b15c82b1990a3a
wzf <627132520@qq.com>
2019-10-26 23:28:09 +0800

    simple

--- gjj/reverse.c
+++ gjj/reverse.c
@@ -1,24 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(int argc, char * argv[])
+int main()
 {
 	long long int n,res = 0;;
-	if (argc != 2)
-	{
-		printf("usage:\t%s [num]\n", argv[0]);return 0;
-	}
-	n = atoll(argv[1]);
-	printf("%lld\t%lld\n",res, n);
-	printf("________________\n|output\t|input\t|\n|_______|_______|\n");
+	scanf("%lld",&n);
 	for (; n > 0LL;n/=10)
 	{
 		res *= 10;
 		res += n % 10;
-		printf("%lld\t|%lld\n",res, n);
 	}
-	
-//	printf("%lld\n",res);
-	
+	printf("%lld\n",res);
 	return 0;
 }
